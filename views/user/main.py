7ff11ea61a30e5dc6be8b6fe0c0178a441783285
neo <g602049338@icloud.com>
2019-10-15 13:39:36 +0800

    厂家基本接口

--- views/user/main.py
+++ views/user/main.py
@@ -1,5 +1,7 @@
+import time
 from views.user import api
 from plugins.HYplugins.common import result_format
+from plugins.HYplugins.common.authorization import login, auth
 from init import wechat_api
 
 
@@ -16,3 +18,39 @@ def sign_in():
     # else:
     #     return common.result_format(error_code=4001, message='客户未注册', data={'open_id': open_id})
     return result_format()
+
+
+@api.route('/refresh_token/')
+@auth.login_required
+def refresh_token():
+    """刷新token"""
+    # iat = g.token_payload.get('iat')
+    # if time.time() - time.mktime(time.localtime(iat)) > 6000:
+    #     user = User.query.filter_by(id=g.user['id']).first()
+    #     return common.result_format(data={'token': user.generate_token(), 'user_info': user.info()})
+    # else:
+    #     return common.result_format(error_code=4008, message='token refresh failure!')
+
+
+@api.route('/registered/', methods=['POST'])
+def registered():
+    """注册成为厂家"""
+
+
+@api.route('/factory/info/')
+@login()
+def factory_info():
+    """厂家信息查询"""
+    # user = User.query.filter_by(id=g.user['id']).first_or_404()
+    # return common.result_format(data=user.info(remove={'genre'}))
+
+
+@api.route('/factory/info/edit/', methods=['POST'])
+@login()
+def factory_info_edit():
+    """厂家信息修改"""
+    # user = User.query.filter_by(id=g.user['id']).first_or_404()
+    # form = validators.FactoryEditForm(user=user).validate_()
+    #
+    # user.set_attrs(form.data).direct_update_()
+    # return common.result_format(data=user.info(remove={'genre'}))
