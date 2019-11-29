b85c4bfe8fbf21b2a298256013bbf1919551dce8
neo <g602049338@icloud.com>
2019-11-29 22:45:00 +0800

    订单地址信息

--- forms/business/fields.py
+++ forms/business/fields.py
@@ -12,7 +12,7 @@ class DescriptionField:
 
     description = wtforms.StringField(validators=[
         DataRequired(message=VM.say('required', '订单详情')),
-        Length(min=255, message=VM.say('length_unite', '订单详情', 255))
+        Length(min=100, message=VM.say('length_unite', '订单详情', 100))
     ])
 
 
