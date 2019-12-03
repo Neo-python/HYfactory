8bb69494603fc668acc4e4255fcd931c59060569
neo <g602049338@icloud.com>
2019-12-03 09:00:17 +0800

    订单地址信息

--- views/user/primary.py
+++ views/user/primary.py
@@ -20,7 +20,7 @@ def sign_in():
     if user:
         return result_format(data={'token': user.generate_token(), 'user_info': user.serialization()})
     else:
-        return result_format(error_code=4000, message='用户未注册')
+        return result_format(error_code=5011, message='用户未注册')
 
 
 @api.route('/refresh_token/')
